07cda70ff549f18fa6ea7155cd78aaf6de161478
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-09-28 16:05:36 -0300

    delete unused turso ts file

--- turso.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@libsql/Client'
-
-const config = {
-    url: process.env.TURSO_DATABASE_URL ?? 'file:local.db',
-    authToken: process.env.TURSO_AUTH_TOKEN,
-}
-
-export const db = createClient(config)
