68e7e479f1b6e133664eca19bed87f82253845aa
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-09-28 15:30:21 -0300

    add cloudinary model, controller

---
+++ models/cloudinary.ts
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from 'cloudinary'
+
+export class CloudinaryModel {
+    static async uploadImage(imageUrl: string, publicName: string) {
+        // Configuration
+        const c = cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.SECRET,
+        })
+
+        // Upload an image
+        const uploadResult = await c.uploader
+            .upload(imageUrl, {
+                public_id: publicName,
+            })
+            .catch((error: any) => {
+                console.log(error)
+            })
+
+        console.log(uploadResult)
+        return uploadResult
+    }
+}
