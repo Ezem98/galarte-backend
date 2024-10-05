931dd23ce55303ee7ae81b0f9c448bae68850397
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-10-04 21:58:30 -0300

    add name & surname

--- schemas/users.ts
+++ schemas/users.ts
@@ -3,6 +3,8 @@ import { ExperienceLevel } from '../enums/experienceLevel.ts'
 import { IUser } from '../types/user.ts'
 
 export const userSchema = z.object({
+    name: z.string().min(2, 'Name must be at least 2 characters'),
+    surname: z.string().min(2, 'Surname must be at least 2 characters'),
     username: z.string().min(6, 'Username must be at least 6 characters'),
     email: z.string().email('Invalid email format'),
     password: z.string().min(8, 'Password must contain at least 8 characters'),
--- types/user.ts
+++ types/user.ts
@@ -2,6 +2,8 @@ import { ExperienceLevel } from '../enums/experienceLevel.ts'
 
 export interface IUser {
     id?: number
+    name: string
+    surname: string
     username: string
     email: string
     password: string
