c16d6f38660205c8782a5f330382449d67342867
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2024-10-27 14:36:49 -0300

    adjust ai for roofs

--- models/openAI.ts
+++ models/openAI.ts
@@ -22,7 +22,11 @@ export class OpenAIModel {
         await this.openai.beta.threads.messages.create(thread.id, {
             role: 'assistant',
             content: `Como especialista en construcción, necesito que escribas una lista paso a paso en formato json con un título y una breve explicación sobre como ${
-                modelCategory == Categories.Opening || modelCategory == Categories.Floor ? 'colocar' : 'construir'
+                modelCategory == Categories.Opening ||
+                modelCategory == Categories.Floor ||
+                modelCategory == Categories.Roof
+                    ? 'colocar'
+                    : 'construir'
             } ${modelName} de ${modelSize.width / 100} metro/s de largo y ${
                 modelSize.height / 100
             } metro/s de alto para una persona con ${
