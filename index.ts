0bf897b57240dbbdf1257a86dee6fa236b93679b
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2025-02-14 13:42:25 -0300

    add allowed origins to cors

--- index.ts
+++ index.ts
@@ -12,7 +12,11 @@ const app = express()
 app.disable('x-powered-by')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(
+    cors({
+        origin: 'https://galartearte.com',
+    })
+)
 app.use(
     fileUpload({
         useTempFiles: true,
