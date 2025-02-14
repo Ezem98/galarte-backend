e369dd74fe728de4894fb1e960630fb0aac1d67d
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2025-02-14 14:10:15 -0300

    quit cors restriction

--- index.ts
+++ index.ts
@@ -12,11 +12,7 @@ const app = express()
 app.disable('x-powered-by')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(
-    cors({
-        origin: 'https://galartearte.com',
-    })
-)
+app.use(cors())
 app.use(
     fileUpload({
         useTempFiles: true,
